fd8cae75f402b41bf6e3f62d1f9ce9db5c30821d
Laura <01laura002@gmail.com>
2023-01-24 15:42:44 -0500

    Initial commit

---
+++ src/main/java/com/laurabuibas/csc325_demo01/Csc325_demo01.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.laurabuibas.csc325_demo01;
+
+/**
+ *
+ * @author laurabuibas
+ */
+public class Csc325_demo01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
---
+++ src/main/java/com/laurabuibas/csc325_demo01/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.laurabuibas.csc325_demo01;
+
+/**
+ *
+ * @author laurabuibas
+ */
+public class NewClass {
+    
+}
